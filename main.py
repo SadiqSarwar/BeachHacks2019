3eec1c570dfc0b23acd3b2c768ce1b6774247240
Sadiq Sarwar <unconfigured@null.spigotmc.org>
2019-04-21 10:46:38 -0700

    WORKING TRANSLATE

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def run_translate():
 
     # Retrieve Translate API's detect_language response for the input text
     form_text = request.form['text']
-    ###detect_language_response = client.detect_language(form_text)
+    detect_language_response = client.detect_language(form_text)
     ###confidence = detect_language_response.get('confidence')
-    input_text = detect_language_response.get('textarea')
+    input_text = detect_language_response.get('text')
     language = detect_language_response.get('language')
 
     # Retrieve Translate API's translate to French response for the input text
@@ -27,9 +27,9 @@ def run_translate():
     translate_response_french = client.translate(form_text, target_language='fr')
     translated_text_french = translate_response_french.get('translatedText')
 
-    return render_template('reviewpage.html', input_text=input_text, language=language, translated_text_french=translated_text_french
+    return render_template('run_translate.html', input_text=input_text, language=language, translated_text_french=translated_text_french)
 
-if __name__ == '__main__':
+if (__name__ == '__main__'):
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run()
